686bf79c17b72189addea2edf74e7cb5b268f0b3
Josh King <jking@chambana.net>
2016-05-22 16:14:17 -0400

    Fix templating.

--- bin/init.sh
+++ bin/init.sh
@@ -7,11 +7,11 @@ CHECK_BIN "dbconfig-generate-include"
 CHECK_BIN "uwsgi"
 
 MSG "Configuring Roundcube..."
-sed -i -e "s/{{ROUNDCUBE_DBUSER/${ROUNDCUBE_DBUSER}/" \
-	-e "s/{{ROUNDCUBE_DBPASS/${ROUNDCUBE_DBPASS}/" \
-	-e "s/{{ROUNDCUBE_DBSERVER/${ROUNDCUBE_DBSERVER}/" \
-	-e "s/{{ROUNDCUBE_DBPORT/${ROUNDCUBE_DBPORT}/" \
-	-e "s/{{ROUNDCUBE_DBNAME/${ROUNDCUBE_DBNAME}/" \
+sed -i -e "s/{{ROUNDCUBE_DBUSER}}/${ROUNDCUBE_DBUSER}/" \
+	-e "s/{{ROUNDCUBE_DBPASS}}/${ROUNDCUBE_DBPASS}/" \
+	-e "s/{{ROUNDCUBE_DBSERVER}}/${ROUNDCUBE_DBSERVER}/" \
+	-e "s/{{ROUNDCUBE_DBPORT}}/${ROUNDCUBE_DBPORT}/" \
+	-e "s/{{ROUNDCUBE_DBNAME}}/${ROUNDCUBE_DBNAME}/" \
 	/etc/dbconfig-common/roundcube.conf
 
 dbconfig-generate-include /etc/dbconfig-common/roundcube.conf > /etc/roundcube/debian-db.php
